e911772921d27473ac26630b2177b7e37bb759c6
petr <petr@snapscore.com>
2021-08-30 15:32:48 +0200

    SPOR-324 Updating also the VertxClientCallbackFactoryImpl.

--- src/main/java/com/snapscore/pipeline/pulling/http/VertxClientCallbackFactoryImpl.java
+++ src/main/java/com/snapscore/pipeline/pulling/http/VertxClientCallbackFactoryImpl.java
@@ -3,23 +3,28 @@ package com.snapscore.pipeline.pulling.http;
 import com.snapscore.pipeline.pulling.FeedRequest;
 import reactor.core.publisher.MonoSink;
 
+import java.util.List;
+
 public class VertxClientCallbackFactoryImpl implements ClientCallbackFactory<VertxClientCallback> {
 
     private final PullingStatisticsService pullingStatisticsService;
     private final int httpResponseBufferSize;
+    private final List<HeadersObserver> headersObservers;
 
     public VertxClientCallbackFactoryImpl(PullingStatisticsService pullingStatisticsService,
-                                          int httpResponseBufferSize) {
+                                          int httpResponseBufferSize,
+                                          List<HeadersObserver> headersObservers) {
         this.pullingStatisticsService = pullingStatisticsService;
         this.httpResponseBufferSize = httpResponseBufferSize;
+        this.headersObservers = headersObservers;
     }
 
     public VertxClientCallbackFactoryImpl(int httpResponseBufferSize) {
-        this(null, httpResponseBufferSize);
+        this(null, httpResponseBufferSize, List.of());
     }
 
     @Override
     public VertxClientCallback createCallback(FeedRequest feedRequest, MonoSink<byte[]> emitter) {
-        return new VertxClientCallbackImpl(pullingStatisticsService, feedRequest, emitter, httpResponseBufferSize);
+        return new VertxClientCallbackImpl(pullingStatisticsService, feedRequest, emitter, httpResponseBufferSize, headersObservers);
     }
 }
