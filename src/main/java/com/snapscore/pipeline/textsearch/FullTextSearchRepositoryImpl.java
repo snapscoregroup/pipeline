28c40e6d58fcafc41b6ca3d88122e8529d1f401c
Janis Tzoumas <tzoumas.janis@gmail.com>
2021-10-05 09:52:28 +0200

    Removing duplicate names from input searchable names

--- src/main/java/com/snapscore/pipeline/textsearch/FullTextSearchRepositoryImpl.java
+++ src/main/java/com/snapscore/pipeline/textsearch/FullTextSearchRepositoryImpl.java
@@ -367,6 +367,7 @@ public class FullTextSearchRepositoryImpl<T extends FullTextSearchableItem> impl
             this.itemWords = upperCaseNames.stream()
                     .flatMap(upperCaseName -> Arrays.stream(SPACE_PATTERN.split(upperCaseName)))
                     .sorted(STRING_COMPARATOR) // IMPORTANT! needs to be sorted for better search performance; the search algorithm relies on the being sorted!
+                    .distinct()
                     .map(word -> word.intern()) // intern all words as there are many repetitions // TODO make interning optional
                     .collect(Collectors.toList());
         }
